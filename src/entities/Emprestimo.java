316a26ecd6729c8314cf0ab44f14bbb52077247a
Carlos <carlos.oli.h23@gmail.com>
2026-02-15 17:42:37 -0300

    Classe Emprestimo feita.

--- src/entities/Emprestimo.java
+++ src/entities/Emprestimo.java
@@ -1,4 +1,70 @@
 package entities;
 
-public class Emprestimo {
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Emprestimo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final Usuario usuario;
+    private final Livro livro;
+    private final LocalDate diaEmprestimo;
+    private LocalDate diaDevolucao;
+
+    public Emprestimo(Usuario usuario, Livro livro, LocalDate diaEmprestimo) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.diaEmprestimo = diaEmprestimo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public Livro getLivro() {
+        return livro;
+    }
+    public LocalDate getDiaEmprestimo() {
+        return diaEmprestimo;
+    }
+
+    public LocalDate getDiaDevolucao() {
+        return diaDevolucao;
+    }
+    public void setDiaDevolucao(LocalDate diaDevolucao) {
+        this.diaDevolucao = diaDevolucao;
+    }
+
+    public boolean devolveuLivro() {
+        return diaDevolucao == null;
+    }
+
+    public String stringDataDevolucao() {
+        if(devolveuLivro()) {
+            return "";
+        } else
+            return "\nDia da Devolução: " + diaDevolucao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Emprestimo that = (Emprestimo) o;
+        return Objects.equals(usuario, that.usuario) && Objects.equals(livro, that.livro);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usuario, livro);
+    }
+
+    @Override
+    public String toString() {
+        return  usuario +
+                "\n" + livro +
+                "\nDia do Empréstimo: " + diaEmprestimo +
+                stringDataDevolucao();
+    }
 }
